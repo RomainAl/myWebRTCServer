36c19e409db97a8c293c8bff7b4a795ad1961925
RomainAl <romaninal@gmail.com>
2022-10-18 23:16:03 +0200

    Server socket io

--- server.js
+++ server.js
@@ -14,10 +14,53 @@ const httpServer = createServer(options, (req, res) => {
 
 const io = new Server(httpServer, {
     cors: {
-      origin: "https://127.0.0.1:5501"
+      origin: "*"
     }
 });
 
+let adminSId;
 io.sockets.on('connection', (socket) => {
-    console.log('nouvel utilisteur');
+    console.log(Date.now(), socket.id, "New user connected");
+
+    socket.on("join", function (roomName, isAdmin) {
+        socket.join(roomName);
+        if (isAdmin) {
+            adminSId = socket.id; 
+            console.log("Is admin :", adminSId)
+            socket.emit("create");
+        } else {
+            console.log("Simple user", socket.id)
+            socket.emit("create");
+        }
+    });
+
+
+    //Triggered when the person who joined the room is ready to communicate.
+    socket.on("ready", function (roomName) {
+        socket.broadcast.to(roomName).emit("ready"); //Informs the other peer in the room.
+    });
+
+    //Triggered when server gets an icecandidate from a peer in the room.
+
+    socket.on("candidate", function (candidate, roomName) {
+        console.log("candidate");
+        socket.broadcast.to(roomName).emit("candidate", candidate); //Sends Candidate to the other peer in the room.
+    });
+
+    //Triggered when server gets an offer from a peer in the room.
+
+    socket.on("offer", function (offer) {
+        socket.to(adminSId).emit("offer", offer, socket.id); //Sends Offer to the other peer in the room.
+    });
+
+    //Triggered when server gets an answer from a peer in the room.
+
+    socket.on("answer", function (answer, clientId) {
+        socket.to(clientId).emit("answer", answer); //Sends Answer to the other peer in the room.
+    });
+
+    socket.on('disconnect', function() {
+        console.log(Date.now(), socket.id, "User has disconnected");
+        io.sockets.emit('disuser', socket.id);
+    });
 });
